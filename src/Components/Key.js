766ddc0cdd04e9dcf826f2ecf2d976c471e76168
Shaun Kamesiepo <skamesiepo@gmail.com>
2023-04-20 15:49:31 +0200

    Added logic for adding and deleting letters

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { AppContext } from '../App';
+import { useContext } from 'react';
 
 
 function Key({ keyVal, bigKey }) {
+  const { onDelete, onSelectLetter, onEnter } = useContext(AppContext);
+
+  const selectLetter = () => {
+    if (keyVal === "ENTER") {
+      onEnter()
+    } else if (keyVal === "DELETE") {
+      onDelete()
+    } else {
+     onSelectLetter(keyVal);
+    }
+  }
   return (
-    <div className="key" id={bigKey && "big"}>
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
       {keyVal}
     </div>
   )
--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useCallback, useEffect} from 'react'
 import Key from './Key';
 
 function Keyboard() {
@@ -6,8 +6,40 @@ function Keyboard() {
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
+  const handleKeyboard = useCallback((event)=> {
+    if (event.key === "Enter"){
+      onEnter();
+    } else if (event.key === "Backspace") {
+      onDelete();
+    } else {
+      keys1.forEach((key)=> {
+        if (event.key === key) {
+          onSelectLetter(key)
+        }
+      })
+      keys2.forEach((key)=> {
+        if (event.key === key) {
+          onSelectLetter(key)
+        }
+      })
+      keys3.forEach((key)=> {
+        if (event.key === key) {
+          onSelectLetter(key)
+        }
+      })
+    }
+  })
+
+  useEffect(()=> {
+    document.addEventListener("keydown", handleKeyboard)
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard)
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div className="keyboard">
+    <div className="keyboard" onKeyDown={handleKeyboard}>
       <div className="line1">{keys1.map((key) => {
         return <Key keyVal={key} />
       })}
